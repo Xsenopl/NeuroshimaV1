74a313abb9260977b4c1e81cef5ad2b0cd566efd
Xsenopl <49074078+Xsenopl@users.noreply.github.com>
2025-05-02 20:24:51 +0200

    Foldery na testy
    Jest:
    - folder na testy i referencje od nich do skryptów

---
+++ Assets/_Scripts/TestForTests.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestForTests
+{
+    int a = 3;
+
+    public int Add(int b)
+    {
+        return a+b;
+    }
+
+}
